3453e55313a932173d396cbd3d2d7dd50dffa003
kirtapos <patrik.andel@seznam.cz>
2022-12-29 20:17:21 +0100

    Example for alerts #1

---
+++ src/Ease/TWB5/Alert.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ease\TWB5;
+
+use Ease\Html\DivTag;
+
+/**
+ * Twitter Bootrstap5 Alert.
+ *
+ * @author    Vitex <vitex@hippy.cz>
+ * @copyright 2019 Vitex@hippy.cz (G)
+ */
+class Alert extends DivTag
+{
+
+    /**
+     * Bootstrap5's Alert
+     * @link https://getbootstrap.com/docs/5.0/components/alerts/
+     *
+     * @param string $type       success|info|warning|danger
+     * @param mixed $content     to insert in
+     * @param array $properties  additional
+     */
+    public function __construct($type, $content = null, $properties = [])
+    {
+        $properties['role'] = 'alert';
+        parent::__construct($content, $properties);
+        $this->addTagClass('alert alert-' . $type);
+    }
+}
