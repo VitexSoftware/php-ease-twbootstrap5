744d2b3899b4989d8a72d135fa52c7658ce9ff60
CyberVitexus <info@vitexsoftware.cz>
2023-05-13 16:22:05 +0200

    Table Class added

---
+++ src/Ease/TWB5/Table.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Twitter Bootstrap5 Table class.
+ *
+ * @author Vitex <vitex@hippy.cz>
+ */
+
+namespace Ease\TWB5;
+
+/**
+ * Description of Table
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+class Table extends \Ease\Html\TableTag
+{
+
+    /**
+     * TWB4 Table.
+     *
+     * @param mixed $content    tag content
+     * @param array $properties table tag options
+     */
+    public function __construct($content = null, $properties = [])
+    {
+        parent::__construct($properties, $content);
+        $this->addTagClass('table');
+    }
+}
