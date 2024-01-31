98156935b62a0236e83c830c629abcecbeef9f65
CyberVitexus <info@vitexsoftware.cz>
2024-01-31 12:51:43 +0100

    Card added more properly

---
+++ src/Ease/TWB5/Card.php
@@ -0,0 +1,23 @@
+<?php
+namespace Ease\TWB5;
+
+use Ease\Html\DivTag;
+
+/**
+ * Description of Card
+ *
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+class Card extends DivTag
+{
+
+    public function __construct($content = null, $properties = [])
+    {
+        if (is_array($properties) && array_key_exists('class', $properties)) {
+            $properties['class'] = 'card '.$properties['class'];
+        } else {
+            $properties['class'] = 'card';
+        }
+        parent::__construct($content, $properties);
+    }
+}
