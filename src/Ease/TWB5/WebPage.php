ca5a37492eaf66337174612f10da53047ea317f6
CyberVitexus <info@vitexsoftware.cz>
2024-01-31 19:17:50 +0100

    Bootstrap Version bump

--- src/Ease/TWB5/WebPage.php
+++ src/Ease/TWB5/WebPage.php
@@ -6,17 +6,18 @@ namespace Ease\TWB5;
  * Stránka TwitterBootstrap5.
  *
  * @author     Vítězslav Dvořák <vitex@hippy.cz>
- * @copyright  2022 Vitex@vitexsoftware.cz (G)
+ * @copyright  2022-2024 info@vitexsoftware.cz (G)
  *
  * @link       https://getbootstrap.com/docs/5.2/getting-started/introduction/
  */
-class WebPage extends \Ease\WebPage {
+class WebPage extends \Ease\WebPage
+{
 
     /**
      * Where to look for bootstrap stylesheet
      * @var string path or url 
      */
-    public $bootstrapCSS = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css';
+    public $bootstrapCSS = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css';
 
     /**
      * Where to look for bootstrap stylesheet theme
@@ -28,9 +29,24 @@ class WebPage extends \Ease\WebPage {
      * Where to look for bootstrap stylesheet scripts
      * @var string path or url 
      */
-    public $bootstrapJavaScript = 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js';
+    public $bootstrapJavaScript = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js';
+
+    /**
+     * 
+     * @var type
+     */
     public $header = null;
+
+    /**
+     * 
+     * @var type
+     */
     public $main = null;
+
+    /**
+     * 
+     * @var type
+     */
     public $footer = null;
 
     /**
@@ -38,7 +54,8 @@ class WebPage extends \Ease\WebPage {
      *
      * @param string   $pageTitle
      */
-    public function __construct($pageTitle = null) {
+    public function __construct($pageTitle = null)
+    {
         parent::__construct($pageTitle);
         Part::twBootstrapize();
 
@@ -48,28 +65,35 @@ class WebPage extends \Ease\WebPage {
         );
     }
 
-    public function addToHeader($content) {
+    public function addToHeader($content)
+    {
         if (is_null($this->header)) {
             $this->header = new \Ease\Html\HeaderTag();
         }
         return $this->header->addItem($content);
     }
 
-    public function addToMain($content) {
+    public function addToMain($content)
+    {
         if (is_null($this->main)) {
             $this->main = new \Ease\Html\MainTag();
         }
         return $this->main->addItem($content);
     }
 
-    public function addToFooter($content) {
+    public function addToFooter($content)
+    {
         if (is_null($this->footer)) {
             $this->footer = new \Ease\Html\FooterTag();
         }
         return $this->footer->addItem($content);
     }
 
-    public function finalize() {
+    /**
+     * Assembly page
+     */
+    public function finalize()
+    {
         if (is_null($this->header) === false) {
             $this->addAsFirst($this->header);
         }
@@ -81,5 +105,4 @@ class WebPage extends \Ease\WebPage {
         }
         parent::finalize();
     }
-
 }
